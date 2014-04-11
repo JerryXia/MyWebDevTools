29c0f042e23a738499b9551ef18d923ce84c7b41
JerryXia <gqkzwy@gmail.com>
2014-04-11 12:16:51 +0800

    add getnewfilms

--- src/popup.js
+++ src/popup.js
@@ -1,214 +1,307 @@
+$.fn.coffee = function(obj){
+  for(var eName in obj)
+    for(var selector in obj[eName])
+      $(this).on(eName, selector, obj[eName][selector]);
+}
+
 /*  Global  */
 $(function(){
-    $('.tab').click(function(){
-        //console.log(this.id);
+  $('#main').coffee({
+    click: {
+      '.menubox .tab': function(){
         $(this).addClass('hover').siblings().removeClass('hover');
         $('#con'+$(this).data('id')).show().siblings().hide();
-        
         switch(parseInt(this.id.substr(3))){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                convertUrl2QR();
-                //console.log(url);
-                break;
-            default:
-                break;
+        case 1:
+          break;
+        case 2:
+          break;
+        case 3:
+          convertUrl2QR();
+          //console.log(url);
+          break;
+        case 4:
+          $container = $('#con4_newfilms');
+          $list = $container.find('div.newfilms');
+          if($list.length > 0){
+            // console.log('1');
+          }else{
+            var html = '';
+            for(var i = 0; i < dylist.length; i++){
+              var tmpl = '<div class="newfilms" data-alias="'+dylist[i].alias+'">'+
+                  '<h2><a href="'+dylist[i].siteurl+'" target="_blank">'+dylist[i].sitename+'</a></h2>'+
+                  '<div class="'+dylist[i].alias+'">加载中....</div></div>';
+              html += tmpl;
+            }
+            $container.append(html);
+            $container.find('div.newfilms').each(function(){
+              var alias = $(this).data('alias');
+              $.get("http://cengshu.apphb.com/api/public/getnewfilm/"+alias, function(res){
+                var resHtml = '<ul>';
+                if(res.length > 0){
+                  for(var i = 0; i < res.length; i++){
+                    resHtml += '<li><a href="'+res[i].Href+'" target="_blank" title="'+res[i].UptTime+'"><nobr>'+ res[i].Txt +'</nobr></a></li>';
+                  }
+                  resHtml += '</ul>';
+                  $('#con4_newfilms div.'+ alias).first().html(resHtml);
+                }else{
+                  $('#con4_newfilms div.'+ alias).first().html('<strong>无数据！</strong>');
+                }
+                
+              });
+            });
+          }
+          break;
+        default:
+          break;
         }
-    });
-    $('#con2_switch').click(function(){
+      },
+      '#con2_switch': function(){
         chrome.tabs.executeScript({
             code: 'document.body.contentEditable=document.body.contentEditable=="true"?"false":"true";document.designMode=document.designMode=="on"?"off":"on";'
         });
-    });
-    $('#con2_bdshoucang').click(function(){
+      },
+      '#con2_bdshoucang': function(){
         chrome.tabs.executeScript({
             code: "(function(d) {var e = d.createElement('script'); e.byebj = true; e.src = 'http://s.wenzhang.baidu.com/js/pjt/content_ex/page/bookmark.js?s=bm&t=' + (+new Date()); var b = d.getElementsByTagName('body')[0]; b.firstChild ? b.insertBefore(e, b.firstChild) : b.appendChild(e); })(document)"
         });
-    });
+      }
+    }
+  });
 });
 
 /*  Coookie  */
-
 $("#qie_name").val(localStorage['qie_name'])
 $("#qie_value").val(localStorage['qie_value'])
 $("#open").click(function(){
-    chrome.tabs.getSelected(undefined, function(tab){
-        chrome.cookies.set({
-            url:tab.url,
-            name:$("#qie_name").val(),
-            value:$("#qie_value").val()
-        }, function(){
-            $("#name").val("")
-            $("#value").val("")
-            chrome.cookies.getAll({
-                url:tab.url
-            },function(cookies){
-                $("#cookies").html("")
-                update(tab.url,cookies)
-            })
-        })
-    });
-    localStorage['qie_name']=$("#qie_name").val()
-     localStorage['qie_value']=$("#qie_value").val()
+  chrome.tabs.getSelected(undefined, function(tab){
+    chrome.cookies.set({
+      url:tab.url,
+      name:$("#qie_name").val(),
+      value:$("#qie_value").val()
+    }, function(){
+      $("#name").val("")
+      $("#value").val("")
+      chrome.cookies.getAll({
+        url:tab.url
+      },function(cookies){
+        $("#cookies").html("")
+        update(tab.url,cookies)
+      })
+    })
+  });
+  localStorage['qie_name']=$("#qie_name").val()
+  localStorage['qie_value']=$("#qie_value").val()
 })
 $("#close").click(function(){
-    chrome.tabs.getSelected(undefined, function(tab){
-        chrome.cookies.remove({
-            url:tab.url,
-            name:$("#qie_name").val(),
-        }, function(){
-            $("#name").val("")
-            $("#value").val("")
-            chrome.cookies.getAll({
-                url:tab.url
-            },function(cookies){
-                $("#cookies").html("")
-                update(tab.url,cookies)
-            })
-        })
-    });
-     localStorage['qie_name']=$("#qie_name").val()
-     localStorage['qie_value']=$("#qie_value").val()
+  chrome.tabs.getSelected(undefined, function(tab){
+    chrome.cookies.remove({
+      url:tab.url,
+        name:$("#qie_name").val(),
+    }, function(){
+      $("#name").val("")
+      $("#value").val("")
+      chrome.cookies.getAll({
+        url:tab.url
+      },function(cookies){
+        $("#cookies").html("")
+        update(tab.url,cookies)
+      })
+    })
+  });
+  localStorage['qie_name']=$("#qie_name").val()
+  localStorage['qie_value']=$("#qie_value").val()
 })
 chrome.tabs.getSelected(undefined, function(tab){
-    chrome.cookies.getAll({
-        url:tab.url
-    },function(cookies){
-        update(tab.url,cookies)
-    })
+  chrome.cookies.getAll({
+    url:tab.url
+  },function(cookies){
+    update(tab.url,cookies)
+  })
 })
 var cache=[
-    
+  
 ]
 var update=function(url,cookies){
-    $.each(cookies,function(i,n){
-        var item=document.createElement("div");
-        
-        var key=document.createElement("input");
-        key.type="text";
-        key.className="name"
-        key.style.width="150px"
-        key.style.marginLeft="10px"
-        key.value=n.name
+  $.each(cookies,function(i,n){
+    var item=document.createElement("div");
+    var key=document.createElement("input");
+    key.type="text";
+    key.className="name"
+    key.style.width="150px"
+    key.style.marginLeft="10px"
+    key.value=n.name
 
-        item.appendChild(key)
-        var value=document.createElement("input");
-        value.type="text";
-        value.className="value"
-        value.style.width="150px"
-        value.style.marginLeft="10px"
-        value.value=n.value
-        item.appendChild(value)
-        var check=document.createElement("a")
-        check.href=""
-        check.innerHTML="更改"
-        item.appendChild(check);
-        var del=document.createElement("a")
-        del.innerHTML="删除"
-        del.href=""
-        item.appendChild(del);
-        $("#cookies").append(item);
-        
-        $(del).click(function(){
-            var name=$(this.parentNode).children(".name").val();
-            var value=$(this.parentNode).children(".value").val()
-            chrome.cookies.remove({
-                url:url,
-                name:$(this.parentNode).children(".name").val()
-            }, function(){
-                cache.push({
-                    name:name,
-                    value:value,
-                    url:url,
-                    isClose:true
-                })
-                chrome.cookies.getAll({
-                    url:url
-                },function(cookies){
-                    $("#cookies").html("")
-                    update(url,cookies)
-                })
-            })
+    item.appendChild(key)
+    var value=document.createElement("input");
+    value.type="text";
+    value.className="value"
+    value.style.width="150px"
+    value.style.marginLeft="10px"
+    value.value=n.value
+    item.appendChild(value)
+    var check=document.createElement("a")
+    check.href=""
+    check.innerHTML="更改"
+    item.appendChild(check);
+    var del=document.createElement("a")
+    del.innerHTML="删除"
+    del.href=""
+    item.appendChild(del);
+    $("#cookies").append(item);
+    
+    $(del).click(function(){
+      var name=$(this.parentNode).children(".name").val();
+      var value=$(this.parentNode).children(".value").val()
+      chrome.cookies.remove({
+        url:url,
+        name:$(this.parentNode).children(".name").val()
+      }, function(){
+        cache.push({
+          name:name,
+          value:value,
+          url:url,
+          isClose:true
         })
-        $(check).click(function(){
-            var name=$(this.parentNode).children(".name").val();
-            var value=$(this.parentNode).children(".value").val()
-            chrome.cookies.set({
-                url:url,
-                name:name,
-                value:value
-            }, function(){
-                chrome.cookies.getAll({
-                    url:url
-                },function(cookies){
-                    $("#cookies").html("")
-                    update(url,cookies)
-                })
-            })
+        chrome.cookies.getAll({
+          url:url
+        },function(cookies){
+          $("#cookies").html("")
+          update(url,cookies)
         })
+      })
     })
+    $(check).click(function(){
+      var name=$(this.parentNode).children(".name").val();
+      var value=$(this.parentNode).children(".value").val()
+      chrome.cookies.set({
+        url:url,
+        name:name,
+        value:value
+      }, function(){
+        chrome.cookies.getAll({
+          url:url
+        },function(cookies){
+          $("#cookies").html("")
+          update(url,cookies)
+        })
+      })
+    })
+  })
 }
 $("#add").click(function(){
-    chrome.tabs.getSelected(undefined, function(tab){
-        chrome.cookies.set({
-            url:tab.url,
-            name:$("#name").val(),
-            value:$("#value").val()
-        }, function(){
-            $("#name").val("")
-            $("#value").val("")
-            chrome.cookies.getAll({
-                url:tab.url
-            },function(cookies){
-                $("#cookies").html("")
-                update(tab.url,cookies)
-            })
-        })
+  chrome.tabs.getSelected(undefined, function(tab){
+    chrome.cookies.set({
+      url:tab.url,
+      name:$("#name").val(),
+      value:$("#value").val()
+    }, function(){
+      $("#name").val("")
+      $("#value").val("")
+      chrome.cookies.getAll({
+        url:tab.url
+      },function(cookies){
+        $("#cookies").html("")
+        update(tab.url,cookies)
+      })
     })
+  })
 })
 $("#value").keydown(function(e){
-    if(e.keyCode!=13) return;
-    chrome.tabs.getSelected(undefined, function(tab){
-        chrome.cookies.set({
-            url:tab.url,
-            name:$("#name").val(),
-            value:$("#value").val()
-        }, function(){
-            $("#name").val("")
-            $("#value").val("")
-            chrome.cookies.getAll({
-                url:tab.url
-            },function(cookies){
-                $("#cookies").html("")
-                update(tab.url,cookies)
-            })
-        })
+  if(e.keyCode!=13) return;
+  chrome.tabs.getSelected(undefined, function(tab){
+    chrome.cookies.set({
+      url:tab.url,
+      name:$("#name").val(),
+      value:$("#value").val()
+    }, function(){
+      $("#name").val("")
+      $("#value").val("")
+      chrome.cookies.getAll({
+        url:tab.url
+      },function(cookies){
+        $("#cookies").html("")
+        update(tab.url,cookies)
+      })
     })
+  })
 })
 
 
 function convertUrl2QR(){
-    chrome.tabs.getSelected(null, function(tab) {
-        var url = "https://chart.googleapis.com/chart?cht=qr"+
-                  "&chs=200x200&choe=UTF-8&chld=Q|0&chl=" + tab.url + "";
+  chrome.tabs.getSelected(null, function(tab) {
+    var url = "https://chart.googleapis.com/chart?cht=qr"+
+          "&chs=200x200&choe=UTF-8&chld=Q|0&chl=" + tab.url + "";
 
-        if (tab.favIconUrl) {
-            $("#fav").attr("src", tab.favIconUrl).show();
-        }
+    if (tab.favIconUrl) {
+      $("#fav").attr("src", tab.favIconUrl).show();
+    }
 
-        $("#fav, #qr").bind("error", function(e) {
-            $(e.target).hide();
-        });
-
-        $("#qr").attr("src", url);
+    $("#fav, #qr").bind("error", function(e) {
+      $(e.target).hide();
     });
+
+    $("#qr").attr("src", url);
+  });
 }
 
+var SimpleTplEngine = function(html, options) {
+  var re = /<%([^%>]+)?%>/g, 
+      reExp = /(^( )?(if|for|else|switch|case|break|{|}))(.*)?/g,
+      code = 'var r=[];\n', 
+      cursor = 0; 
+  var add = function(line, isjs) { 
+    isjs ? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') : 
+          (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
+    return add;
+  } 
+  while(match = re.exec(html)) { 
+      add(html.slice(cursor, match.index))(match[1], true); 
+      cursor = match.index + match[0].length; 
+  } 
+  add(html.substr(cursor, html.length - cursor)); 
+  code += 'return r.join("");'; // <-- return the result 
+  //console.log(code);
+  return new Function(code.replace(/[\r\t\n]/g, '')).apply(options); 
+}
 
+var dylist = [
+  {
+    sitename: "飘花电影",
+    siteurl: "http://www.piaohua.com",
+    alias: "piaohua",
+    charset: "utf-8"
+  },
+  {
+    sitename: "放放电影",
+    siteurl: "http://www.poxiao.com",
+    alias: "ffdy",
+    charset: "gb2312"
+  },
+  {
+    sitename: "6v电影",
+    siteurl: "http://www.6vdy.com",
+    alias: "6vdy",
+    charset: "gb2312"
+  },
+  {
+    sitename: "大片网",
+    siteurl: "http://www.bigpian.cn",
+    alias: "bigpian",
+    charset: "utf-8"
+  },
+  {
+    sitename: "经典电影",
+    siteurl: "http://www.tbmovie.com",
+    alias: "tbmovie",
+    charset: "utf-8"
+  }
+];
 
-
+var getDyListItem = function(dylist, alias){
+  for(var i = 0; i < dylist.length; i++){
+    if(dylist[i].alias == alias)
+      return dylist[i];
+  }
+  return null;
+}
