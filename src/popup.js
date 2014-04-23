534993a1065d84ca9027fa861c8fcd85399e5a26
JerryXia <gqkzwy@gmail.com>
2014-04-23 17:34:56 +0800

    add markdown preview

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,5 @@
+//"use strict";
+
 $.fn.coffee = function(obj){
   for(var eName in obj)
     for(var selector in obj[eName])
@@ -64,6 +66,28 @@ $(function(){
       '#con2_bdshoucang': function(){
         chrome.tabs.executeScript({
             code: "(function(d) {var e = d.createElement('script'); e.byebj = true; e.src = 'http://s.wenzhang.baidu.com/js/pjt/content_ex/page/bookmark.js?s=bm&t=' + (+new Date()); var b = d.getElementsByTagName('body')[0]; b.firstChild ? b.insertBefore(e, b.firstChild) : b.appendChild(e); })(document)"
+        });
+      },
+      '#con2_btn-export': function(){
+        chrome.tabs.getSelected(null, function(tab) {
+          chrome.tabs.sendRequest(tab.id, {method: "getHtml"}, function(response) {
+            if(response.method=="getHtml"){
+              var html = response.data;
+
+              var urlObject = window.URL || window.webkitURL || window,
+                builder = new Blob([html], {type: 'text/plain; charset=utf-8'});
+
+              var saveLink = document.createElementNS("http://www.w3.org/1999/xhtml", "a")
+              saveLink.href = urlObject.createObjectURL(builder);
+              saveLink.download = 'export.html';
+              var event = document.createEvent('MouseEvents');
+              event.initMouseEvent(
+                "click", true, false, window, 0, 0, 0, 0, 0
+                , false, false, false, false, 0, null
+              );
+              saveLink.dispatchEvent(event);
+            }
+          });
         });
       }
     }
@@ -228,6 +252,57 @@ $("#value").keydown(function(e){
 })
 
 
+
+/******************
+* markdown preview
+*******************/
+var pageKey, 
+  storage = chrome.storage.local,
+  themePrefix = 'theme_',
+  specialThemePrefix = 'special_',
+  defaultThemes = ['Clearness', 'ClearnessDark', 'Github', 'TopMarks'];
+
+storage.get('theme', function(items) {
+  var theme = items.theme ? items.theme : 'Clearness';
+  $('#current-theme').html(theme);
+});
+
+// theme
+function getThemes() {
+  storage.get(['custom_themes', pageKey], function(items) {
+    if(items.custom_themes) {
+      var k, v, themes = items.custom_themes;
+      var group = $('<optgroup label="Custom themes"></optgroup>');
+
+      $('#theme optgroup[label="Custom themes"]').empty().remove();
+      for(k in themes) {
+        v = themes[k];
+        group.append($("<option></option>").text(v)); 
+      }
+      $('#theme').append(group);
+    }
+
+    if(items[pageKey]) {
+      $('#theme').val(items[pageKey]);
+    } 
+  });
+}
+
+chrome.tabs.getSelected(null, function(tab) {
+  pageKey = specialThemePrefix + tab.url;
+  getThemes();
+  $('#theme').change(function() {
+    var obj = {};
+    obj[pageKey] = $(this).val();
+    storage.set(obj);
+  });
+});
+
+
+
+/************************
+* url convert to qrcode
+************************/
 function convertUrl2QR(){
   chrome.tabs.getSelected(null, function(tab) {
     var url = "https://chart.googleapis.com/chart?cht=qr"+
