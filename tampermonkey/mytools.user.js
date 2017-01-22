0b99fa0be07290a3db3602f591cfd1126f15514a
JerryXia <gqkzwy@gmail.com>
2017-01-22 15:12:25 +0800

    update
    Signed-off-by: JerryXia <gqkzwy@gmail.com>

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -4,7 +4,7 @@
 // @name:zh-TW         MyTools：我的私有工具集
 // @namespace          http://www.guqiankun.com/
 // @homepageURL        https://greasyfork.org/scripts/10453-mytools
-// @version            1.1.4
+// @version            1.1.5
 // @description        整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向，默认快捷键：ALT + M
 // @description:zh-CN  整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向，默认快捷键：ALT + M
 // @description:zh-TW  整合常用功能，減少插件數量：DirectGoogle、百度音樂盒去廣告、豆瓣補全下載鏈接、網頁右鍵解鎖、購物黨比價工具、解決百度雲大文件下載限制、知乎界面美化、知乎真實鏈接地址重定向，默認快捷鍵：ALT + M
@@ -454,44 +454,30 @@ var GmUtils = (function () {
             setTimeout(replaceLink, 7000);
         }
         function videoFuck(){
-            var w = $('#playerBox').width();
-            var h = $('#playerBox').height() - 50;
-            var iframeSrc = 'https://api.47ks.com/webcloud/?url='+ encodeURIComponent(location.href);
-            $('#module_basic_player').html('<iframe src="{0}" border="0" width="{1}" height="{2}"></iframe>', iframeSrc, w, h);
-
-            //嵌入式视频0
-            var flagVideo = $('embed').each(function(i, v){
-                var $this = $(v);
-                var src = $this.attr('src');
-                if(src && src.length > 0){
-                    var match = src.match(/^http:\/\/player.youku.com\/player.php\/sid\/(.+)\/v.swf/i);
-                    if(match && match.length == 2){
-                        var w = $this.width();
-                        var h = $this.height() - 50;
-                        var youkuUrl = String.format('http://v.youku.com/v_show/id_{0}.html', match[1]);
-                        var iframeSrc = 'https://api.47ks.com/webcloud/?url='+ encodeURIComponent(youkuUrl);
-                        $(String.format('<iframe src="{0}" border="0" width="{1}" height="{2}"></iframe>', iframeSrc, w, h)).insertAfter($this);
-                        $this.remove();
+            if($('#module_basic_player').length > 0){
+                var w = $('#playerBox').width();
+                var h = $('#playerBox').height() - 50;
+                var iframeSrc = 'https://api.47ks.com/webcloud/?url='+ encodeURIComponent(location.href);
+                // http://v.youku.com/v_show/id_XMjQ3ODQ0MzQwNA==.html
+                $('#module_basic_player').html(String.format('<iframe src="{0}" border="0" width="{1}" height="{2}"></iframe>', iframeSrc, w, h));
+            }else{
+                //嵌入式视频1
+                var flagVideo = $('embed').each(function(i, v){
+                    var $this = $(v);
+                    var src = $this.attr('src');
+                    if(src && src.length > 0){
+                        var match = src.match(/sid\/(.+)\/v.swf/i);
+                        if(match && match.length == 2){
+                            var w = $this.width();
+                            var h = $this.height() - 50;
+                            var youkuUrl = String.format('http://v.youku.com/v_show/id_{0}.html', match[1]);
+                            var iframeSrc = 'https://api.47ks.com/webcloud/?url='+ encodeURIComponent(youkuUrl);
+                            $(String.format('<iframe src="{0}" border="0" width="{1}" height="{2}"></iframe>', iframeSrc, w, h)).insertAfter($this);
+                            $this.remove();
+                        }
                     }
-                }
-            });
-            //嵌入式视频1
-            var flagVideo = $('embed').each(function(i, v){
-                var $this = $(v);
-                var src = $this.attr('src');
-                if(src && src.length > 0){
-                    var match = src.match(/sid\/(.+)\/v.swf/i);
-                    if(match && match.length == 2){
-                        var w = $this.width();
-                        var h = $this.height() - 50;
-                        var youkuUrl = String.format('http://v.youku.com/v_show/id_{0}.html', match[1]);
-                        var iframeSrc = 'https://api.47ks.com/webcloud/?url='+ encodeURIComponent(youkuUrl);
-                        $(String.format('<iframe src="{0}" border="0" width="{1}" height="{2}"></iframe>', iframeSrc, w, h)).insertAfter($this);
-                        $this.remove();
-                    }
-                }
-            });
-
+                });
+            }
             console.log('隐藏功能在作怪。');
         }
 
