575942bda1efbdc809e456e76c6d589802e31dcc
JerryXia <gqkzwy@gmail.com>
2017-02-06 22:13:43 +0800

    Signed-off-by: JerryXia <gqkzwy@gmail.com>

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -22,10 +22,10 @@
 // @exclude            http://bbs.htpc1.com/*
 // @exclude            *://exmail.qq.com/*
 // @exclude            *://cas.edu.sh.cn/*
-// @require            http://7xrmpf.com1.z0.glb.clouddn.com/js/jquery/jquery.min.js
-// @require            http://7xrmpf.com1.z0.glb.clouddn.com/js/jquery/jquery.qrcode.min.js
-// @require            http://7xrmpf.com1.z0.glb.clouddn.com/js/mustache/mustache.min.js
-// @require            http://7xrmpf.com1.z0.glb.clouddn.com/js/mts/vue.A04E3C7A70CF46E4A76FC260775187CD.js
+// @require            https://7xrmpf.com1.z0.glb.clouddn.com/js/jquery/jquery.min.js
+// @require            https://7xrmpf.com1.z0.glb.clouddn.com/js/jquery/jquery.qrcode.min.js
+// @require            https://7xrmpf.com1.z0.glb.clouddn.com/js/mustache/mustache.min.js
+// @require            https://7xrmpf.com1.z0.glb.clouddn.com/js/mts/vue.A04E3C7A70CF46E4A76FC260775187CD.js
 // @grant              unsafeWindow
 // @grant              GM_log
 // @grant              GM_addStyle
