a8482a03821148dc079b9017a9d022faa75017a4
JerryXia <gqkzwy@gmail.com>
2017-02-11 15:20:46 +0800

    update

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -4,12 +4,13 @@
 // @name:zh-TW         MyTools：我的私有工具集
 // @namespace          https://greasyfork.org/scripts/10453-mytools
 // @homepageURL        https://greasyfork.org/scripts/10453-mytools
-// @version            1.3.33
+// @version            1.3.34
 // @description        整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），默认快捷键：ALT + M
 // @description:zh-CN  整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），默认快捷键：ALT + M
 // @description:zh-TW  整合常用功能，減少插件數量：DirectGoogle、百度音樂盒去廣告、豆瓣補全下載鏈接、網頁右鍵解鎖、購物黨比價工具、解決百度雲大文件下載限制、知乎界面美化、知乎真實鏈接地址重定向、全網主流視頻網站VIP破解（免廣告），默認快捷鍵：ALT + M
 // @author             JerryXia
 // @icon               http://tampermonkey.net/favicon.ico
+// @supportURL         http://blog.guqiankun.com/contact
 // @license            The MIT License (MIT); http://opensource.org/licenses/MIT
 // @match              *://*/*
 // @include            /^https?\:\/\/(www|news|maps|docs|cse|encrypted)\.google\./
