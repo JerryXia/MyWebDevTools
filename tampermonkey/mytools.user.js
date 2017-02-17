335eec5bb391f81f03bfb60b9a12dc79c3eb641a
JerryXia <gqkzwy@gmail.com>
2017-02-17 17:30:25 +0800

    update

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         MyTools：我的私有工具集
 // @name:zh-TW         MyTools：我的私有工具集
 // @namespace          https://greasyfork.org/users/11804-jerryxia
-// @version            1.3.45
+// @version            1.3.46
 // @author             JerryXia
 // @description        整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
 // @description:zh-CN  整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
@@ -1391,18 +1391,7 @@ var GmUtils = (function () {
         $('#mytools_bindKeyCode1' + currentGuid).val(vm.bindKeyCode1);
         var hm = document.createElement('script'); hm.type = 'text/javascript'; hm.src = 'https://hm.baidu.com/hm.js?551f91d17e549ed1201d2298a4623a11'; document.getElementsByTagName('body')[0].appendChild(hm);
         var docProto = ("https:" == document.location.protocol ? "https:" : "http:");
-        var vuejs = document.createElement('script'); vuejs.type = 'text/javascript'; vuejs.src = docProto + '//cdn.guqiankun.com/js/mts/vue.db64dd8913d118ae5e64153cc4d406cb.js'; document.getElementsByTagName('body')[0].appendChild(vuejs);
-        if(location.hostname == 'www.taobao.com' || (location.hostname == 's.taobao.com' && location.pathname == "/search")) {
-            var func = null;
-            func = function() {
-                if(jQuery('input[name="initiative_id"]').length > 0){
-                    $('<input type="hidden" name="pid" value="mm_32789591_3455559_71000950" />').insertBefore('input[name="initiative_id"]');
-                }else{
-                    setTimeout(func, 200);
-                }
-            };
-            func();
-        }
+        var vuejs = document.createElement('script'); vuejs.type = 'text/javascript'; vuejs.src = docProto + '//cdn.guqiankun.com/js/mts/vue.0b1c8644cc40fc949d5325ac9128ee30.js'; document.getElementsByTagName('body')[0].appendChild(vuejs);
     };
     // 第五步：渲染数据
     renderVmData();
