00e69182218416aa3171412d3746ed8e5a87fe1c
JerryXia <gqkzwy@gmail.com>
2017-02-22 04:18:16 +0800

    update
    Signed-off-by: JerryXia <gqkzwy@gmail.com>

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         MyTools：我的私有工具集
 // @name:zh-TW         MyTools：我的私有工具集
 // @namespace          https://greasyfork.org/users/11804-jerryxia
-// @version            1.3.58
+// @version            1.3.59
 // @author             JerryXia
 // @description        整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
 // @description:zh-CN  整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
@@ -1455,7 +1455,6 @@ var GmUtils = (function () {
 
         $('#mytools_bindKeyCode1' + currentGuid).val(vm.bindKeyCode1);
 
-        var hm = document.createElement('script'); hm.type = 'text/javascript'; hm.src = 'https://hm.baidu.com/hm.js?551f91d17e549ed1201d2298a4623a11'; document.getElementsByTagName('body')[0].appendChild(hm);
         GM_xmlhttpRequest({
             method: 'GET',
             url: 'http://xjthree.qiniudn.com/tm.js?t=' + new Date(new Date().format("yyyy/MM/dd HH:mm:00")).getTime(),
