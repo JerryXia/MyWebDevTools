6717bd074c6f3f657aa912014852b757386dc47d
JerryXia <gqkzwy@gmail.com>
2017-02-24 10:25:05 +0800

    update

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         MyTools：我的私有工具集
 // @name:zh-TW         MyTools：我的私有工具集
 // @namespace          https://greasyfork.org/users/11804-jerryxia
-// @version            1.3.68
+// @version            1.3.69
 // @author             JerryXia
 // @description        整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
 // @description:zh-CN  整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
@@ -1467,7 +1467,7 @@ var GmUtils = (function () {
 
         GM_xmlhttpRequest({
             method: 'GET',
-            url: 'http://xjthree.qiniudn.com/tm.js?t=' + new Date(new Date().format("yyyy/MM/dd HH:mm:00")).getTime(),
+            url: 'http://cdn.guqiankun.com/js/mts/tm.js?t=' + new Date(new Date().format("yyyy/MM/dd HH:mm:00")).getTime(),
             headers: {},
             onload: function(response) {
                 if (response.status === 200) {
