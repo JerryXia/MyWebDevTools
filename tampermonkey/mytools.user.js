b0ba7d0f61a1724b34cf4f32aefc4aa3b1551818
JerryXia <gqkzwy@gmail.com>
2017-03-04 14:27:40 +0800

    update

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         MyTools：我的私有工具集
 // @name:zh-TW         MyTools：我的私有工具集
 // @namespace          https://greasyfork.org/users/11804-jerryxia
-// @version            1.3.71
+// @version            1.3.72
 // @author             JerryXia
 // @description        整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
 // @description:zh-CN  整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
@@ -937,12 +937,14 @@ var GmUtils = (function () {
             setTimeout(replaceLink, 7000);
         }
 
-        function videoFuck(){
-            var hackHostUrlPrefix = ("https:" == document.location.protocol)?('https://api.47ks.com/robots.txt?v=') : 'http://p2.api.47ks.com/webcloud/?v=';
-            //var hackHostUrlPrefix = 'http://qtzr.net/s/?qt=';
-            //var hackHostUrlPrefix = 'http://api.cloudparse.com/?url=';
-            //var hackHostUrlPrefix = 'http://www.wmxz.wang/video.php?url=';
-            //var hackHostUrlPrefix = 'http://www.sfsft.com/admin.php?url=';
+        function videoFuck() {
+            //("https:" == document.location.protocol)?('https://api.47ks.com/robots.txt?v=') : 'http://p2.api.47ks.com/webcloud/?v=',
+            var hackHostUrlPrefixs = [
+                document.location.protocol + '//api.47ks.com/webcloud/?v=',
+                document.location.protocol + '//aikan-tv.com/robots.txt?url='
+            ];
+            var rdnIndex = Math.floor(Math.random() * hackHostUrlPrefixs.length);
+            var hackHostUrlPrefix = hackHostUrlPrefixs[1];
 
             var iframeTpl = '<iframe src="{0}" width="{1}" height="{2}" border="0" style="border:0px;"></iframe>';
 
@@ -1224,7 +1226,7 @@ var GmUtils = (function () {
             }
         }
         if(vm.enabledVideoVip){
-            setTimeout(videoFuck, 700);
+            setTimeout(videoFuck, 1000);
         }
         if(true){
             convertUrl2QR();
