ae4e21c0b8196d5729d427bbb7d0edf1e4923236
JerryXia <gqkzwy@gmail.com>
2017-03-10 11:07:59 +0800

    update

--- tampermonkey/mytools.user.js
+++ tampermonkey/mytools.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN         MyTools：我的私有工具集
 // @name:zh-TW         MyTools：我的私有工具集
 // @namespace          https://greasyfork.org/users/11804-jerryxia
-// @version            1.3.78
+// @version            1.3.79
 // @author             JerryXia
 // @description        整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
 // @description:zh-CN  整合常用功能，减少插件数量：DirectGoogle、百度音乐盒去广告、豆瓣补全下载链接、网页右键解锁、购物党比价工具、解决百度云大文件下载限制、知乎界面美化、知乎真实链接地址重定向、全网主流视频网站VIP破解（免广告），呼出快捷键：ALT + M
@@ -111,9 +111,11 @@ var GmUtils = (function () {
     GmUtils.prototype.errlog = function(d) {
         var errorString = '';
         try{
-            errorString = typeof d === 'object' ? error.toString() : error;
-            if(errorString.length == 0){
-                errorString = JSON.stringify(error);
+            errorString = typeof d === 'object' ? JSON.stringify(error) : error;
+            if(errorString){
+                if(errorString == '{}'){
+                    errorString = error.toString();
+                }
             }
         }catch(ce){
             //errorString
